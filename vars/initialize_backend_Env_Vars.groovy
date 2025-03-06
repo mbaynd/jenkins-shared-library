46d882212dae789fb3b4864c5260349b25d93cf9
Mbaye NDIAYE <mbaynd@gmail.com>
2025-03-06 17:02:47 +0000

    Update to take care of Scheduler Rest

--- vars/initialize_backend_Env_Vars.groovy
+++ vars/initialize_backend_Env_Vars.groovy
@@ -13,10 +13,8 @@ def call(String environ, String project_image, String image_tag) {
     env.KPAY_WEB_APP_SERVICE_NAME = "smartpay-web-"+environ
     env.KPAY_MIG_APP_SERVICE_NAME = "smartpay-mig-"+environ
 
-
     env.KPAY_APP_NETWORK = "smartpay-net-" + environ
     env.KPAY_SCHED_APP_NETWORK = "smartpay-net-rest-sched" + environ
-
 
     env.KPAY_APP_REPLICAS = 1 
     env.KPAY_APP_LOKI_BASE_URL = "http://10.0.12.211:3100"
@@ -34,25 +32,22 @@ def call(String environ, String project_image, String image_tag) {
     if (environ == "uat" || environ == "dev") {
 
         environ = "uat"
-        env.KPAY_APP_ENV_LABEL = "monit"
+        env.KPAY_APP_ENV_LABEL = environ
         env.KPAY_AWS_REGION = "us-east-1"
         env.KPAY_AWS_ECR = '688149143527.dkr.ecr.us-east-1.amazonaws.com'
 
+        
         env.KPAY_REST_APP_IMAGE = "kpay-rest"
-        env.KPAY_REST_SCHED_APP_IMAGE = "kpay-rest-scheduler"
-
-        env.KPAY_REST_APP_SUBNET = "192.168.191.0/24"
         env.KPAY_REST_APP_PORT = "8780"
 
+        env.KPAY_REST_SCHED_APP_IMAGE = "kpay-rest-scheduler"
+        env.KPAY_REST_SCHED_APP_PORT = "8790"
+
         env.KPAY_WEB_APP_IMAGE = "kpay-web"
-        env.KPAY_WEB_APP_SUBNET = "192.168.191.0/24"
         env.KPAY_WEB_APP_PORT = "8781"
 
         env.KPAY_MIG_APP_IMAGE = "migration-app"
-        env.KPAY_MIG_APP_SUBNET = "192.168.191.0/24"
-        env.KPAY_MIG_APP_PORT = "9780"
-
-        //env.KPAY_REST_APP_HEALTHCHECK_URL = "http://localhost:3000/v1/doc"
+        env.KPAY_MIG_APP_PORT = "9781"
     
     } 
 
@@ -62,20 +57,16 @@ def call(String environ, String project_image, String image_tag) {
         env.KPAY_AWS_ECR = '688149143527.dkr.ecr.eu-west-1.amazonaws.com'
      
         env.KPAY_REST_APP_IMAGE = "smartpay-rest"
-        env.KPAY_REST_SCHED_APP_IMAGE = "smartpay-rest-scheduler"
-        env.KPAY_REST_APP_SUBNET = "192.168.191.0/24"
         env.KPAY_REST_APP_PORT = "8080"
 
+        env.KPAY_REST_SCHED_APP_IMAGE = "smartpay-rest-scheduler"
+        env.KPAY_REST_SCHED_APP_PORT = "8890"
+
         env.KPAY_REST_APP_IMAGE = "smartpay-web"
-        env.KPAY_REST_APP_SUBNET = "192.168.192.0/24"
         env.KPAY_REST_APP_PORT = "8081"
 
         env.KPAY_WEB_APP_IMAGE = "smartpay-migration"
-        env.KPAY_WEB_APP_SUBNET = "192.168.193.0/24"
         env.KPAY_WEB_APP_PORT = "19081"
-
-     
-        //env.KPAY_REST_APP_HEALTHCHECK_URL = "http://localhost:3000/v1/doc"
     
     }
 
@@ -87,16 +78,16 @@ def call(String environ, String project_image, String image_tag) {
      
 
         env.KPAY_REST_APP_IMAGE = "kpay-backend/smartpay-rest"
-        env.KPAY_REST_SCHED_APP_IMAGE = "smartpay-rest-scheduler"
-        env.KPAY_REST_APP_SUBNET = "192.168.191.0/24"
         env.KPAY_REST_APP_PORT = "18780"
 
+        env.KPAY_REST_SCHED_APP_IMAGE = "smartpay-rest-scheduler"
+        env.KPAY_REST_SCHED_APP_PORT = "18890"
+
+
         env.KPAY_REST_APP_IMAGE = "kpay-backend/smartpay-web"
-        env.KPAY_REST_APP_SUBNET = "192.168.192.0/24"
         env.KPAY_REST_APP_PORT = "18780"
 
         env.KPAY_WEB_APP_IMAGE = "smartpay-migration"
-        env.KPAY_WEB_APP_SUBNET = "192.168.193.0/24"
         env.KPAY_WEB_APP_PORT = "19081"
 
                 
