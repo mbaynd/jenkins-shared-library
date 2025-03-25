e43ca0930db5a6690e6673d0f0c492ffece3a35d
Mbaye NDIAYE <mbaynd@gmail.com>
2025-03-25 16:11:21 +0000

    update

--- vars/sendBuildNotification.groovy
+++ vars/sendBuildNotification.groovy
@@ -31,7 +31,7 @@ def call(Map args = [:]) {
 
 // Function to convert milliseconds to human-readable format
 def formatDuration(millis) {
-    long duration = millis as long // Ensure millis is a long type
+    long duration = millis.toLong() // Ensure it is a long before performing any math
     long seconds = (duration / 1000) % 60
     long minutes = (duration / (1000 * 60)) % 60
     long hours = (duration / (1000 * 60 * 60))
