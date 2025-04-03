1ba03e030c1d93663e660f8fdcf8c81222290f64
Mbaye NDIAYE <mbaynd@gmail.com>
2025-04-03 12:20:30 +0000

    update

--- vars/sendBuildNotification.groovy
+++ vars/sendBuildNotification.groovy
@@ -16,12 +16,13 @@ def call(Map args = [:]) {
         subject: "${env.ENV}: ${env.JOB_BASE_NAME} --> ${currentBuild.result}",
         body: """<html>
             <center><p style='border: 1px solid blue; padding: 5px; align: center;'><strong>Project: ${env.JOB_NAME}</strong></p></center>
+            <p><strong>Build Number:</strong> ${env.BUILD_NUMBER}</p>
             <p><strong>Start build time:</strong> ${env.START_BUILD_DATETIME}</p>
             <p><strong>End build time:</strong> ${env.END_BUILD_DATETIME}</p>
             <p><strong>Build Duration:</strong> ${currentBuild.duration} secondes</p>
             <p><strong>Build User Name:</strong> ${env.BUILD_USER}</p>
             <p><strong>Build User Email:</strong> ${env.BUILD_USER_EMAIL}</p>
-            <p><strong>Build Number:</strong> ${env.BUILD_NUMBER}</p>
+            
             <p><strong>URL:</strong> <a href="${env.BUILD_URL}">${env.BUILD_URL}</a></p>
         </html>""",
         from: fromEmail,
